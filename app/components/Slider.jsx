d917185b5b3223bdeeaad43b0b7ec96f8500fd04
rohanverma25 <rohanverma25dec@gmail.com>
2026-06-05 12:16:42 +0530

    Optimize

--- app/components/Slider.jsx
+++ app/components/Slider.jsx
@@ -4,7 +4,7 @@ export default function Slider() {
         <>
         <section className="slider">
             <img className="desktop" fetchPriority='high' alt="Banner" width="1520" height="507" decoding="async" data-nimg="1" srcSet="/images/Do-you-know-why-divine-is-trusted.webp 1x, /images/Do-you-know-why-divine-is-trusted.webp 2x" src="/images/Do-you-know-why-divine-is-trusted.webp"></img>
-            <img className="mobile" fetchPriority='high' alt="Banner" width="375" height="125" decoding="async" data-nimg="1" srcSet="/images/Do-you-know-why-divine-is-trusted.webp 1x, /images/Do-you-know-why-divine-is-trusted.webp 2x" src="/images/Do-you-know-why-divine-is-trusted.webp"></img>
+            {/* <img className="mobile" fetchPriority='high' alt="Banner" width="375" height="125" decoding="async" data-nimg="1" srcSet="/images/Do-you-know-why-divine-is-trusted.webp 1x, /images/Do-you-know-why-divine-is-trusted.webp 2x" src="/images/Do-you-know-why-divine-is-trusted.webp"></img> */}
 
         </section>
         </>
